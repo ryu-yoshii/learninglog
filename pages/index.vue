25ad151fafc547a8c8000ea9ec58a44d4ffeb853
ryu-yoshii <dgy15044@gmail.com>
2021-11-01 07:28:57 +0900

    11/1

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title class="font-weight-bold">タイトル</v-card-title>
+      <v-card-text>
+        テキストテキストテキスト <br>
+        テキストテキストテキスト
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="primary" block>ボタン</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss">
+  
+</style>
